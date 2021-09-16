406348355051bc03549de5f31dc367191a947324
Zach Kellen <z.kellen12@gmail.com>
2021-09-16 07:44:16 -0500

    validations added

--- server/models/jokes.model.js
+++ server/models/jokes.model.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const JokeSchema = new mongoose.Schema({
-    setup: String,
-    punchline: String
+    setup: {type: String, required: [true, "Set up is required."], minlength: [10, "Setup must be at least 10 characters."]},
+    punchline: {type: String, required: [true, "Punchline is required."], minlength: [3, "Punchline must be at least 3 characters."]}
 }, {timestamps: true})
 
 const Joke = mongoose.model('Joke', JokeSchema);
